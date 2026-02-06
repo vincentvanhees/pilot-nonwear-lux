12343b46ee36a4259c0f89bdd629ac6e46da2d76
Vincent van Hees <8362333+vincentvanhees@users.noreply.github.com>
2026-02-06 13:48:44 +0100

    update scripts to use new wrapper function applyClassifyNonwear

--- pilot_with_VEET.R
+++ pilot_with_VEET.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 graphics.off()
 
 library(tidyverse)
@@ -6,6 +7,10 @@ library(gt)
 library(ggridges)
 library(downlit)
 library(magick)
+
+source(paste0(getwd(), "/classifyNonwear.R"))
+source(paste0(getwd(), "/applyClassifyNonwear.R"))
+
 
 path <- "D:/Projects/Spitschan/01_VEET_L.csv.zip"
 tz   <- "US/Central"
@@ -32,54 +37,59 @@ rm(dataIMU)
 wear_data <- wear_data |> select(Id, wear, start, end)
 
 # Load light sensor data
-dataVEET <- import$VEET(path, tz = tz, modality = "ALS", manual.id = "VEET", silent = TRUE)
+VEET <- import$VEET(path, tz = tz, modality = "ALS", manual.id = "VEET", silent = TRUE)
 
 # Select relevant columns
-dataVEET <- dataVEET[, c("time_stamp", "Datetime", "Lux")]  #"integration_time"
+VEET <- VEET[, c("time_stamp", "Datetime", "Lux")]  #"integration_time"
 
 # Aggregate
-dataVEET <- dataVEET |>
+VEET <- VEET |>
   aggregate_Datetime(unit = "5 seconds") |>
   gap_handler(full.days = TRUE) |>
   add_Date_col(group.by = TRUE) |>
   remove_partial_data(Lux, threshold.missing = "1 hour")
 
 # Add wear column
-dataVEET$nonwear = TRUE
+VEET$nonwear = TRUE
 for (i in 1:nrow(wear_data)) {
   if (wear_data$wear[i] == TRUE) {
-    wear = which(dataVEET$Datetime >= wear_data$start[i] & dataVEET$Datetime <= wear_data$end[i])
+    wear = which(VEET$Datetime >= wear_data$start[i] & VEET$Datetime <= wear_data$end[i])
     print(length(wear))
     if (length(wear) > 0) {
-      dataVEET$nonwear[wear] = FALSE
+      VEET$nonwear[wear] = FALSE
     }
   }
 }
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-print(table(dataVEET$nonwear))
+
+# Classify nonwear
+
+filename = "01_VEET_L.csv.zip"
+
+# Prepare data:
+# change column names to match expectation by function, to do: align with default in LightLogR
+data = VEET[, c("Lux", "Datetime", "nonwear")]
+data = data[order(VEET$Datetime),]
+
+# For comparison later on, create numeric non-wear column to ease plotting
+# where 0 is wear and 1 is nonwear
+VEET$nonwear = as.numeric(VEET$nonwear)
+# make sure nonwear column has NA when Lux has NA
+is.na(VEET$nonwear[is.na(VEET$Lux)]) = TRUE
 
 # change column names to match expectation by function, to do: align with default in LightLogR
-data = dataVEET[, c("Lux", "Datetime")]
-colnames(data) = c("lux", "time")
-# data$lux[is.na(data$lux)] = 0
-source(paste0(getwd(), "/classifyNonwear.R"))
-data = classifyNonwear(data,
-                       resolution_seconds = 60, # for computational only derive statistics at 1 minute resolution
-                       N_days_required_daily_stats = 3,
-                       minimum_relval_per_hour = 0.1, #minimum relative variance per hour
-                       epoch_size = 5)
+VEET = VEET[, c("Datetime", "Lux", "nonwear")]
+colnames(VEET)[3] = "nonwear_ref"
+
+# Run classifier
+out = applyClassifyNonwear(VEET, # assumed to have columns time and Lux, and represent a continuous regular time series
+                           resolution_seconds = 60, # for computational only deride statistics at 1 minute resolution
+                           N_days_required_daily_stats = 3,
+                           minimum_relval_per_hour = 0.1, #minimum relative variance per hour
+                           epoch_size = 5, # epoch size in seconds
+                           lowLuxThreshold = 50, # Lux below this value is considered closed to zero
+                           maxLowLuxSequenceHours = 16,
+                           plot_path = "D:/Projects/Spitschan",
+                           plot_id = filename) # max number of hours with low Lux
 
 
-# Crude plot to inspect classification
-graphics.off()
-x11()
-par(mfrow = c(4,2), mar = c(3, 3, 3, 1))
-plot(dataVEET$Datetime, dataVEET$Lux, type = "l", main ="lux")
-plot(dataVEET$Datetime, dataVEET$nonwear, type = "l", main = "reference nonwear", ylim = c(0, 1))
-lines(data$time, data$nonwear_estimate * 0.9, type = "l", col = "blue")
-plot(data$time, data$nonwearA, type = "l", main = "nonwear A", ylim = c(0, 1))
-plot(data$time, data$nonwearB, type = "l", main = "nonwear B")
-plot(data$time, data$nonwearC, type = "l", main = "nonwear C")
-plot(data$time, data$nonwearD, type = "l", main = "nonwear D")
-plot(data$time, data$nonwearE, type = "l", main = "nonwear F")
-plot(data$time, data$nonwearF, type = "l", main = "nonwear F")
